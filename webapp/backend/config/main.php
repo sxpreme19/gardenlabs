8d0e67cfeb81105894cca234f31454aca56dd71e
tomasmonteiro <2231659@my.ipleiria.pt>
2024-10-25 15:45:10 +0100

    adminlte yii2

--- webapp/backend/config/main.php
+++ webapp/backend/config/main.php
@@ -37,6 +37,13 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        'view' => [
+         'theme' => [
+             'pathMap' => [
+                '@app/views' => '@vendor/hail812/yii2-adminlte3/src/views'
+                ],
+            ],
+        ],
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
